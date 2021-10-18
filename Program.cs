15f7ad1b55b2e150b40ab00d823ffe2966fd8855
masiuko@ukr.net <masiuko@ukr.net>
2021-10-18 19:04:54 +0300

    Oleh Masiuk

--- Program.cs
+++ Program.cs
@@ -8,11 +8,10 @@ namespace SigmaTask7
         {
             Task1.Vocabulary voc = new Task1.Vocabulary();
 
-            //string text2 = "She. Dotn e likes     ... ?  nature. I go to school.";
+            string text2 = "She. Dotn e likes     ... ?  nature. I go to school.";
 
-            //Console.WriteLine("Curent text:\n{0}", text2);
-            //Console.WriteLine("Changed text:\n{0}", voc.ChangeText(text2));
-
+            Console.WriteLine("Curent text:\n{0}", text2);
+            Console.WriteLine("Changed text:\n{0}", voc.ChangeText(text2));
             Console.WriteLine("\nOur vocabulary:\n {0}",voc.ToString());
 
             string pricePath = @"C:\Users\Acer\OneDrive\Робочий стіл\C#\SigmaTask7\Task2\Prices.txt";
